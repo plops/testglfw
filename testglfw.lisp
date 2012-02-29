a74e8aff5a4bd1b03e328a31df7e696b6a91a7af
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-02-29 09:46:37 +0100

    draw rectangle

--- testglfw.lisp
+++ testglfw.lisp
@@ -52,33 +52,47 @@
 	   t0 t1))
    (incf frames)))
 
+(defun draw-tetraeder ()
+  (gl:color-3ub 255 255 255)
+  (gl:line-width 1)
+  (gl:with-begin gl:+line-loop+
+    (loop for (x y z) in '((0 0 1) (0 1 0) (1 0 0)
+			   (1 1 1) (0 0 1) (0 1 0)) do
+	 (gl:vertex-3f x y z))))
+
 (let ((rot 0))
  (defun draw ()
    (destructuring-bind (w h) (glfw:get-window-size)
     (setf h (max h 1))
     (gl:viewport 0 0 w h)
+    (gl:clear-color .0 .2 .2 1)
     (gl:clear gl:+color-buffer-bit+)
     (gl:matrix-mode gl:+projection+)
     (gl:load-identity)
     (glu:perspective 65 (/ w h) 1 100)
     (gl:matrix-mode gl:+modelview+)
     (gl:load-identity)
-    (glu:look-at 0 0 14
-		 0 20 0
+    (glu:look-at 0 20 14 ;; camera
+		 0 0 0 ;; target
 		 0 0 1))
-   (gl:translate-f 0 20 0)
+   (gl:translate-f 0 0 0)
    (gl:rotate-f rot 0 0 1)
    (if (< rot 360)
        (incf rot .3)
        (setf rot 0))
    (draw-grid)
    (count-fps)
-   (gl:color-3ub 255 255 255)
    (gl:line-width 1)
+   (gl:color-3f 1 1 1)
+   (let ((s 10))
+    (gl:scale-d s s s))
    (gl:with-begin gl:+line-loop+
-     (loop for (x y z) in '((0 0 1) (0 1 0) (1 0 0)
-			    (1 1 1) (0 0 1) (0 1 0)) do
-	  (gl:vertex-3f x y z)))))
+     (gl:vertex-2f 0 0)
+     (gl:vertex-2f 0 1)
+     (gl:vertex-2f 1 1)
+     (gl:vertex-2f 1 0)
+     )
+   ))
 
 (glfw:do-window (:title "bla" :width 512 :height 512)
     ()

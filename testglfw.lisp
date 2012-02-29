a973889ed5874e53e9b42d6bfb18fd8af0e2cb2c
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-02-29 16:17:53 +0100

    i can see texture from my data now, but not as expected

--- testglfw.lisp
+++ testglfw.lisp
@@ -80,16 +80,16 @@
     (if (< rot 360)
 	(incf rot .3)
 	(setf rot 0))
-    ;(draw-grid)
-    ;(count-fps)
-    ;(gl:line-width 3)
-    ;(gl:color-3f 1 1 1)
-    ;(gl:enable gl:+lighting+)
-    ;(gl:enable gl:+depth-test+)
-    ;(gl:enable gl:+light0+)
-    ;(gl:shade-model gl:+flat+)
-    ;(gl:light-fv gl:+light0+ gl:+position+ #(1s0 4s0 2s0 1s0))
-    ;(gl:material-fv gl:+front+ gl:+ambient-and-diffuse+ #(0.9 0.9 0.0 1.0))
+    (draw-grid)
+    (count-fps)
+    (gl:line-width 3)
+    (gl:color-3f 1 1 1)
+    (gl:enable gl:+lighting+)
+    (gl:enable gl:+depth-test+)
+    (gl:enable gl:+light0+)
+    (gl:shade-model gl:+flat+)
+    (gl:light-fv gl:+light0+ gl:+position+ #(1s0 4s0 2s0 1s0))
+    (gl:material-fv gl:+front+ gl:+ambient-and-diffuse+ #(0.9 0.9 0.0 1.0))
     #+nil
     (gl:with-push-matrix 
       (gl:translate-f 0 0 .08)
@@ -105,31 +105,34 @@
     (gl:with-push-matrix
       (gl:rotate-f rot 0 0 1)
       (gl:material-fv gl:+front+ gl:+ambient-and-diffuse+ 
-		      (make-array 4 :initial-element .7s0 :element-type 'single-float))
-      (let* ((s .15)
+		      (make-array 4 :initial-element 1s0 :element-type 'single-float))
+      (let* ((s .5)
 	     (h 40)
 	     (w 40)
-	     (ww 32)
-	     (hh 32)
-	     (img (make-array (list hh ww) :element-type '(unsigned-byte 8)))
+	     (ww 256)
+	     (hh 256)
+	     (img (make-array (list 4 hh ww) :element-type '(unsigned-byte 8)))
 	     (objs (make-array 1 :element-type '(unsigned-byte 64))))
 	(unless (glfw:load-texture-2d 
 		 "C:/Users/martin/quicklisp/dists/quicklisp/software/cl-glfw-20110730-git/examples/mipmaps.tga" 
 		 glfw:+build-mipmaps-bit+)
 	  (break "unable to load texture"))
-	;(gl:pixel-store-i gl:+unpack-alignment+ 1)
+	(gl:pixel-store-i gl:+unpack-alignment+ 1)
 	;(gl:gen-textures (length objs) objs)
 	;(gl:bind-texture gl:+texture-2d+ (aref objs 0))
 	(gl:tex-parameter-i gl:+texture-2d+ gl:+texture-min-filter+ gl:+linear-mipmap-linear+)
        (gl:tex-parameter-i gl:+texture-2d+ gl:+texture-mag-filter+ gl:+linear+)
        (gl:enable gl:+texture-2d+)
 	;(defparameter *img* (list objs img))
-	#+nil(dotimes (i ww)
+	(dotimes (i ww)
 	  (dotimes (j hh)
-	    (setf (aref img j i) (if (oddp (+ i j)) 0 255))))
-	#+nil (sb-sys:with-pinned-objects (img)
-	  (gl:tex-image-2d gl:+texture-2d+ 0 gl:+luminance8+ ww hh 0
-			   gl:+luminance+ gl:+unsigned-byte+
+	    (setf (aref img 0 j i) (if (oddp (* i j)) i 255)
+		  (aref img 1 j i) (if (oddp (* i j)) j 255)
+		  (aref img 2 j i) (if (oddp (* i j)) 0 255)
+		  (aref img 3 j i) (if (oddp (* i j)) 0 255))))
+	(sb-sys:with-pinned-objects (img)
+	  (gl:tex-image-2d gl:+texture-2d+ 0 gl:+rgba+ ww hh 0
+			   gl:+rgba+ gl:+unsigned-byte+
 			   (sb-sys:vector-sap 
 			    (sb-ext:array-storage-vector img))))
 	
@@ -138,28 +141,28 @@
 	    (format t "get-error: ~a~%" a)))
 	(gl:scale-f s s s)
 	(gl:translate-f (* w -.5) (* h -.5) .1)
-
+	#+nil
 	(gl:with-begin gl:+quads+
 	  (gl:tex-coord-2f -1.0  1.0) (gl:vertex-3f -50.0 0.0 -50.0)
 	  (gl:tex-coord-2f  1.0  1.0) (gl:vertex-3f  50.0 0.0 -50.0)
 	  (gl:tex-coord-2f  1.0 -1.0) (gl:vertex-3f  50.0 0.0  50.0)
 	  (gl:tex-coord-2f -1.0 -1.0) (gl:vertex-3f -50.0 0.0  50.0))
 	
-	#+nil(gl:with-begin gl:+quads+
+	(gl:with-begin gl:+quads+
 	  (dotimes (j h)
-	    (let ((d -.2))
+	    (let ((d 0 ;-.2
+		    ))
 	      (dotimes (i w)
-		(gl:normal-3f 0 0 s)
-		(gl:tex-coord-2f 0 0)
-		(gl:vertex-2f i j)
-		(gl:tex-coord-2f 0 1)
-		(gl:vertex-2f i (+ d 1 j))
-		(gl:tex-coord-2f 1 1)
-		(gl:vertex-2f (+ d 1 i) (+ d 1 j))
-		(gl:tex-coord-2f 1 0)
-		(gl:vertex-2f (+ d 1 i) j)))))
-	;(gl:disable gl:+lighting+)
-					;(gl:disable gl:+texture-2d+)
+		(labels ((c (a b)
+			   (gl:tex-coord-2f (* a (/ 1s0 w)) (* b (/ 1s0 h)))
+			   (gl:vertex-2f a b)))
+		 (gl:normal-3f 0 0 s)
+		 (c i j)
+		 (c i (+ d 1 j))
+		 (c (+ d 1 i) (+ d 1 j))
+		 (c (+ d 1 i) j))))))
+	(gl:disable gl:+lighting+)
+	(gl:disable gl:+texture-2d+)
 	;(gl:delete-textures 1 objs)
 	))))
 
